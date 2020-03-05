b369687aaf97385d76082a74ff7fd630c2de14bc
ROmanFedun <fero5uin@gmail.com>
2020-03-06 00:00:01 +0200

    get started with ajax

--- app.component.ts
+++ app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularServer';
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = [];
+
+  todoTitle = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+     .subscribe( todos => {
+       console.log('Response', todos);
+       console.log('http', this.http);
+       this.todos = todos;
+     })
+    }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    } 
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+    
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe( todo => {
+        console.log('todo', todo);
+        this.todos.unshift(todo);
+        this.todoTitle = '';
+      })
+    
+  
+
+  }
 }
+
